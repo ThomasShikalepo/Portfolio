6c1a3da67fd63fd459828dd617965f02cb93ec5d
Thomas Shikalepo <thomasshikalepo@gmail.com>
2025-08-20 15:57:02 +0200

    coding is a craft

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/hero'
+import About from './sections/About'
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       < Navbar />
       <Hero/>
+      <About/>
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
+       
       {/*about*/}
       {/*projects*/}
       {/*testimonial*/}
